d34fabc478c372a4dc65c4211a2c4bfd45474697
tech-in-tech <anubhavsharma06dec@gmail.com>
2025-06-12 09:47:27 +0530

    setup frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary">Secondary</button>
+    </>
+  )
+}
+
+export default App
